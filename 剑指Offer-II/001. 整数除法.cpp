0f4fe66a88e931811af11c4a11de95e05053ba56
hewei-bit <1003826976@qq.com>
2022-04-27 01:42:57 +0800

    剑指offer

--- 剑指Offer-II/001. 整数除法.cpp
+++ 剑指Offer-II/001. 整数除法.cpp
@@ -21,5 +21,22 @@ class Solution
 public:
     int divide(int a, int b)
     {
+        if (a == INT_MIN && b == -1)
+            return INT_MAX;
+        int sign = (a > 0) ^ (b > 0) ? -1 : 1;
+
+        if (a > 0)
+            a = -a;
+        if (b > 0)
+            b = -b;
+
+        unsigned int res = 0;
+        while (a <= b)
+        {
+            a -= b;
+            res++;
+        }
+
+        return sign == 1 ? res : -res;
     }
 };
