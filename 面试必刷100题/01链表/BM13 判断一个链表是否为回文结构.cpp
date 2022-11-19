8d1dc1115d77332e8a42ce1216c42fecc0ca88f3
hewei-bit <1003826976@qq.com>
2022-11-20 00:40:49 +0800

    刷题大业20221120 必刷13

--- 面试必刷100题/01链表/BM13 判断一个链表是否为回文结构.cpp
+++ 面试必刷100题/01链表/BM13 判断一个链表是否为回文结构.cpp
@@ -24,3 +24,50 @@ struct ListNode
     {
     }
 };
+
+class Solution
+{
+public:
+    ListNode *Reverse(ListNode *head)
+    {
+        // 前序节点
+        ListNode *prev = NULL;
+        while (head != NULL)
+        {
+            ListNode *tmp = head->next;
+            head->next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+    bool isPail(ListNode *head)
+    {
+        // 空链表直接为回文
+        if (head == NULL)
+        {
+            return true;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head;
+        // 双指针找中点
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // 中点处反转
+        slow = Reverse(slow);
+        fast = head;
+        while (slow != NULL)
+        {
+            if (slow->val != fast->val)
+            {
+                return false;
+            }
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+};
